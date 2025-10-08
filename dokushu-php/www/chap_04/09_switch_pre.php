54e5d108356d0c25cf6256aeb349d024e8b378ea
Hiroki Ckok <hrk.ckok36@gmail.com>
2025-10-09 04:30:56 +0900

    add if命令 多岐分岐（switch命令 変換前）

---
+++ dokushu-php/www/chap_04/09_switch_pre.php
@@ -0,0 +1,11 @@
+<?php
+$rank = '甲';
+if ($rank == '甲') {
+  echo '大変良いです。';
+} elseif ($rank == '乙') {
+  echo '良いです';
+} elseif ($rank == '丙') {
+  echo 'もう少し頑張りましょう。';
+} else {
+  echo '？？？';
+}
