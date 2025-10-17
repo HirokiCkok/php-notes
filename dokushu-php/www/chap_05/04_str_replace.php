d801dcb0ce8e2597f30741ccac49abdf0b62b5a2
Hiroki Ckok <hrk.ckok36@gmail.com>
2025-10-17 09:00:00 +0900

    add str_replace関数 - 部分文字列を置換する

--- dokushu-php/www/chap_05/04_str_replace.php
+++
@@ -1,5 +0,0 @@
-<?php
-$str = 'WINGSプロジェクト';
-echo mb_strstr($str, 'S', true), '<br>';
-echo mb_strstr($str, 'S'), '<br>';
-echo mb_strstr($str, 'M', false), '<br>';
---
+++ dokushu-php/www/chap_05/05_str_replace1.php
@@ -0,0 +1,4 @@
+<?php
+$str = 'にわにはにわにわとりがいる';
+echo str_replace('にわ', 'ニワ', $str, $cnt). '<br />';
+echo "{$cnt}個の置き換えをしました。";
