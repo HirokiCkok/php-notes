a82dd8bad2687bb8b3b57e6863bd3119c5a6b606
Hiroki Ckok <hrk.ckok36@gmail.com>
2025-10-26 19:44:59 +0900

    update リファクタリング

--- dokushu-php/www/chap_06/20_static3.php
+++ dokushu-php/www/chap_06/20_static3.php
@@ -2,10 +2,10 @@
 function checkStatic(): void {
   static $x = 0;
   $x++;
-  echo "unset前：{$x}, \n";
+  echo "unset前：{$x} \n";
   unset($x);
   $x = 13;
-  echo "unset後：{$x}, \n";
+  echo "unset後：{$x} \n";
 }
 
 checkStatic();
