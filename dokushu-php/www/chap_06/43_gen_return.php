6991200bee06a883180e60e330139f9e23979f42
Hiroki Ckok <hrk.ckok36@gmail.com>
2025-11-09 17:52:47 +0900

    add ジェネレーターの結果を取得

---
+++ dokushu-php/www/chap_06/43_gen_return.php
@@ -0,0 +1,22 @@
+<?php
+function readLines(string $path) {
+  $i = 0;    // 行数
+  $file = fopen($path, 'rb') or die('ファイルが見つかりません');
+  // 行単位にテキストを取得&yield
+  while($line = fgets($file, 1024)) {
+    $i++;
+    yield $line;
+  }
+  fclose($file);
+  // 読み込んだテキストの行数を返す
+  return $i;
+}
+
+$br = (PHP_SAPI === 'cli') ? "\n" : "<br />";
+// sample.datから行単位にテキストを出力
+$gen = readLines(__DIR__ . '/43s_sample.dat');
+foreach($gen as $line) {
+  echo $line. $br;
+}
+// 最終的に読み込んだ行数を取得
+echo  "{$gen->getReturn()}行ありました。";
