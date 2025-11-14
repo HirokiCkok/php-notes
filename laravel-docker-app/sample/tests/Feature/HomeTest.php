0e300442ebfd22899fe3c90ac244e276ddfbc3d7
Hiroki Ckok <hrk.ckok36@gmail.com>
2025-11-14 22:15:41 +0900

    add トップ画面のテスト用コード

--- laravel-docker-app/sample/tests/Feature/HomeTest.php
+++ laravel-docker-app/sample/tests/Feature/HomeTest.php
@@ -8,13 +8,15 @@ use Tests\TestCase;
 
 class HomeTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    public function testStatusCode()
     {
-        $response = $this->get('/');
-
+        $response = $this->get('/home');
         $response->assertStatus(200);
+    }
+
+    public function testBody()
+    {
+        $response = $this->get('/home');
+        $response->assertSeeText("こんにちは！");
     }
 }
