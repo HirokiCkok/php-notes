554879a50cb4db770286c85d4059760dddf18b29
Hiroki Ckok <hrk.ckok36@gmail.com>
2025-11-23 08:00:00 +0900

    add HTTP通信：Guzzleの基本的なサンプル

---
+++ dokushu-php/www/chap_07/16_http_basic.php
@@ -0,0 +1,11 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+// クライアントを生成
+$cli = new GuzzleHttp\Client([
+  'base_uri' => 'https://wings.msn.to/',
+]);
+// リクエストを送信
+$res = $cli->request('get', '/tmp/sample.txt');
+// 取得したコンテンツを出力
+echo $res->getBody();
