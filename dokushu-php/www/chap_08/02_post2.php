a12c6ee04b9dfce716926c7a2d0efddf599bb38b
Hiroki Ckok <hrk.ckok36@gmail.com>
2025-11-26 08:00:00 +0900

    add ソースコードを元に戻す

--- dokushu-php/www/chap_08/02_post2.php
+++ dokushu-php/www/chap_08/02_post2.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8" />
     <title>ポストデータ</title>
   </head>
-  <?php require_once '03_Encode.php'; ?>
   <body>
-    こんにちは、<?=e($_POST['name']) ?>さん！
+    こんにちは、<?= $_POST['name'] ?>さん！
   </body>
 </html>
